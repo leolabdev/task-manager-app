d667b12034bf4ea15be03bd6b4d3bc1fbbc80ccb
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-03-02 18:32:27 +0200

    init project

---
+++ frontend/src/app/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+        test
+    </div>
+  )
+}
+
+export default App
