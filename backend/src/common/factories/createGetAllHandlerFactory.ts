228a8c5c117e7a4b0e0118293ba0683791148fd8
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-03-06 10:26:56 +0200

    add

--- backend/src/common/factories/createGetAllHandlerFactory.ts
+++ backend/src/common/factories/createGetAllHandlerFactory.ts
@@ -1,14 +1,14 @@
 import {validateGetAllPaginationQuery} from "@/common/validation/validateGetAllPaginationQuery";
 import {NextFunction,Request,Response} from "express";
 import HttpException from "@/common/http-exception";
-import logger from "@/common/logger";
 
 /**
  * Create a handler function to get all items.
  * @param {object} service - The service to use for getting all items.
+ * @param {Function} logger - The logger function to use for logging errors.
  * @returns {Function} A handler function that can be used to get all items.
  */
-export function createGetAllHandler(service: any): (req: Request, res: Response, next: NextFunction) => Promise<void> {
+export function createGetAllHandler(service: any, logger: any): (req: Request, res: Response, next: NextFunction) => Promise<void> {
     return async (req: Request, res: Response, next: NextFunction): Promise<void> => {
         try {
             const { error, value } = validateGetAllPaginationQuery(req.query);
@@ -20,13 +20,15 @@ export function createGetAllHandler(service: any): (req: Request, res: Response,
             const { page = 1, limit = 10 } = value;
             const data = await service.getAll(Number(page), Number(limit));
 
+
             if (!data) {
                 throw new HttpException(404, "Data not found");
             }
 
             res.status(200).json(data);
         } catch (error: unknown) {
-            logger.error(error);
+
+            logger(error);
 
             if (error instanceof HttpException) {
                 if (error.status != null) {
