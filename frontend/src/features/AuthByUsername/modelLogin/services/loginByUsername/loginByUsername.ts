e11a2dc1f212a6466283b554b0057f92c4567264
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-03-08 13:54:18 +0200

    add validation

--- frontend/src/features/AuthByUsername/modelLogin/services/loginByUsername/loginByUsername.ts
+++ frontend/src/features/AuthByUsername/modelLogin/services/loginByUsername/loginByUsername.ts
@@ -1,7 +1,19 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import {User, userActions} from '@/entities/User';
+import {loginActions} from "../../slice/loginSlice";
+import {object, string, ValidationError} from "yup";
 
+const LoginByUsernameSchema = object().shape({
+    username: string().required('Username is required')
+        .min(3, 'Username must be at least 3 characters')
+        .max(10, 'Username cannot exceed 10 characters')
+        .matches(/^[a-zA-Z0-9]+$/, 'Username can only contain alphanumeric characters'),
+    password: string().required('Password is required')
+        .min(6, 'Password must be at least 6 characters')
+        .max(30, 'Password cannot exceed 30 characters')
+        .matches(/^[a-zA-Z0-9]+$/, 'Password can only contain alphanumeric characters'),
+});
 
 interface LoginByUsernameProps {
     username: string;
@@ -12,19 +24,30 @@ export const loginByUsername = createAsyncThunk<User, LoginByUsernameProps, { re
     'login/loginByUsername',
     async (authData, thunkAPI) => {
         try {
+            // Validate authData using the LoginByUsernameSchema
+            await LoginByUsernameSchema.validate(authData);
+
             const response = await axios.post<User>(`${import.meta.env.VITE_API}/login`, authData);
 
             if (!response.data) {
                 throw new Error();
             }
             thunkAPI.dispatch(userActions.setAuthData(response.data));
+            thunkAPI.dispatch(loginActions.clearForm());
 
             return response.data;
         } catch (e) {
-            if(e.response && e.response.status === 400) {
-                return thunkAPI.rejectWithValue('Invalid username or password');
+            if (e instanceof ValidationError) {
+                // If authData is not valid, return validation error using rejectWithValue
+                return thunkAPI.rejectWithValue(e.errors[0]);
             }
-          return thunkAPI.rejectWithValue("Server error, try again later");
+            if (e.response && e.response.status === 400) {
+                return thunkAPI.rejectWithValue(
+                    'Invalid username or password'
+                );
+            }
+            return thunkAPI.rejectWithValue('Server error, try again later');
         }
     },
 );
+
--- frontend/src/features/AuthByUsername/modelRegister/services/registerByUsername/registerByUsername.ts
+++ frontend/src/features/AuthByUsername/modelRegister/services/registerByUsername/registerByUsername.ts
@@ -1,27 +1,53 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+import { object, string , ValidationError } from 'yup';
+
+const RegisterByUsernameSchema = object().shape({
+    username: string().required('Username is required')
+        .min(3, 'Username must be at least 3 characters')
+        .max(10, 'Username cannot exceed 10 characters')
+        .matches(/^[a-zA-Z0-9]+$/, 'Username can only contain alphanumeric characters'),
+    password: string().required('Password is required')
+        .min(6, 'Password must be at least 6 characters')
+        .max(30, 'Password cannot exceed 30 characters')
+        .matches(/^[a-zA-Z0-9]+$/, 'Password can only contain alphanumeric characters'),
+});
 
 interface RegisterByUsernameProps {
     username: string;
     password: string;
 }
 
-export const registerByUsername = createAsyncThunk<RegisterByUsernameProps, { rejectValue: string}>(
-    'register/registerByUsername',
-    async (authData, thunkAPI) => {
-        try {
-            const response = await axios.post(`${import.meta.env.VITE_API}/register`, authData);
+export const registerByUsername = createAsyncThunk<
+    RegisterByUsernameProps,
+    { rejectValue: string }
+    >('register/registerByUsername', async (authData, thunkAPI) => {
+    try {
+        // Validate authData using the RegisterByUsernameSchema
+        await RegisterByUsernameSchema.validate(authData);
 
-            if (!response.data) {
-                throw new Error();
-            }
+        const response = await axios.post(
+            `${import.meta.env.VITE_API}/register`,
+            authData
+        );
 
-            return response.data;
-        } catch (e) {
-            if(e.response && e.response.status === 409) {
-                return thunkAPI.rejectWithValue('User with this username already exists');
-            }
-          return thunkAPI.rejectWithValue("Server error, try again later");
+        if (!response.data) {
+            throw new Error();
         }
-    },
-);
+
+        return response.data;
+    } catch (e) {
+        if (e instanceof ValidationError) {
+            // If authData is not valid, return validation error using rejectWithValue
+            return thunkAPI.rejectWithValue(e.errors[0]);
+        }
+
+        if (e.response && e.response.status === 409) {
+            return thunkAPI.rejectWithValue(
+                'User with this username already exists'
+            );
+        }
+        return thunkAPI.rejectWithValue('Server error, try again later');
+    }
+});
+
