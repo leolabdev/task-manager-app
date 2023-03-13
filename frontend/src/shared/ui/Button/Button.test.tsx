f158f5db2c35a587d3a3fa9ea3ec9957c75d26ea
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-03-13 18:02:40 +0200

    add exstra testes

--- frontend/src/shared/ui/Button/Button.test.tsx
+++ frontend/src/shared/ui/Button/Button.test.tsx
@@ -1,18 +1,50 @@
-import {render, screen} from "@testing-library/react";
-import {Button, ButtonTheme} from "./Button";
+import {fireEvent, render, screen} from "@testing-library/react";
+import {Button, ButtonSize, ButtonTheme} from "./Button";
 
 describe("Button", () => {
-  it("test render", () => {
-   render(<Button>Test</Button>);
-   expect(screen.getByText('Test')).toBeInTheDocument();
-  });
+    it("renders with children", () => {
+        render(<Button>Test</Button>);
+        expect(screen.getByText('Test')).toBeInTheDocument();
+    });
 
-  // console.log(ButtonTheme.CLEAR)
+    it("renders with a custom theme classname", () => {
+        render(<Button theme={ButtonTheme.CLEAR}>Test</Button>);
+        expect(screen.getByText('Test')).toHaveClass('clear');
+    });
 
-  it("should have a theme classname", () => {
-    render(<Button theme={ButtonTheme.CLEAR}>Test</Button>);
-    expect(screen.getByText('Test')).toHaveClass('clear');
-    // screen.debug();
-  });
+    it("renders with a custom size classname", () => {
+        render(<Button  size={ButtonSize.L}>Test</Button>);
+        expect(screen.getByText('Test')).toHaveClass('sizeL');
+    });
+
+    it("renders with square styling", () => {
+        render(<Button square>Test</Button>);
+        expect(screen.getByText('Test')).toHaveClass('square');
+    });
+
+    it("renders as disabled", () => {
+        render(<Button disabled>Test</Button>);
+        expect(screen.getByText('Test')).toBeDisabled();
+    });
+
+    it("calls onClick handler when clicked", () => {
+        const handleClick = jest.fn();
+        render(<Button onClick={handleClick}>Test</Button>);
+        fireEvent.click(screen.getByText('Test'));
+        expect(handleClick).toHaveBeenCalledTimes(1);
+    });
+
+    it("applies additional className", () => {
+        render(<Button className="custom">Test</Button>);
+        expect(screen.getByText('Test')).toHaveClass('custom');
+    });
+
+    it("renders with default props", () => {
+        render(<Button>Test</Button>);
+        expect(screen.getByText('Test')).toHaveClass('Button', 'sizeM');
+        expect(screen.getByRole('button')).not.toBeDisabled();
+        expect(screen.getByRole('button')).not.toHaveClass('square');
+    });
 
 });
+
