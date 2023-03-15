829844c347dbadce718f8c5011196ca71cd98d2c
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-03-15 19:15:22 +0200

    fix test, add action

--- frontend/src/shared/ui/Modal/Modal.test.tsx
+++ frontend/src/shared/ui/Modal/Modal.test.tsx
@@ -1,4 +1,12 @@
-import {fireEvent, queryByTestId, render, screen, waitFor, waitForElementToBeRemoved} from "@testing-library/react";
+import {
+    act,
+    fireEvent,
+    queryByTestId,
+    render,
+    screen,
+    waitFor,
+    waitForElementToBeRemoved
+} from "@testing-library/react";
 import { Modal } from "./Modal";
 
 const ANIMATION_DELAY = 300;
@@ -9,25 +17,15 @@ describe("Modal component", () => {
         expect(getByText("Hello World!")).toBeInTheDocument();
     });
 
-    // todo fix this test
-    // it("does not render the children when isOpen is false", async () => {
-    //     const { getByText, queryByText } = render(
-    //         <Modal isOpen={false}>Hello World!</Modal>
-    //     );
-    //     const element = getByText("Hello World!");
-    //     expect(element).toBeInTheDocument();
-    //     fireEvent.click(screen.getByTestId("overlay"));
-    //     await waitForElementToBeRemoved(() => queryByText("Hello World!"));
-    //     expect(queryByText("Hello World!")).not.toBeInTheDocument();
-    // });
-
     it("calls onClose when the overlay is clicked", () => {
         jest.useFakeTimers();
         const handleClose = jest.fn();
         render(<Modal isOpen={true} onClose={handleClose}>Hello World!</Modal>);
         const overlay = screen.getByTestId("overlay");
         fireEvent.click(overlay);
-        jest.advanceTimersByTime(ANIMATION_DELAY);
+        act(() => {
+            jest.advanceTimersByTime(ANIMATION_DELAY);
+        });
         expect(handleClose).toHaveBeenCalled();
         jest.useRealTimers();
     });
@@ -37,11 +35,12 @@ describe("Modal component", () => {
         const handleClose = jest.fn();
         render(<Modal isOpen={true} onClose={handleClose}>Hello World!</Modal>);
         fireEvent.keyDown(document, { key: "Escape" });
-        jest.advanceTimersByTime(ANIMATION_DELAY);
+        act(() => {
+            jest.advanceTimersByTime(ANIMATION_DELAY);
+        });
         expect(handleClose).toHaveBeenCalled();
         jest.useRealTimers();
     });
-
 
     it("does not render the Modal when lazy and not mounted", () => {
         const { queryByTestId } = render(<Modal lazy={true} isOpen={false} />);
